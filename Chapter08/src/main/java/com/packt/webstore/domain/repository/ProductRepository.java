0fccd74da9527e848617f6954e6206dfe1788ee4
Vu Duc Chinh <chinhcn3@gmail.com>
2017-11-24 15:23:41 +0700

    update chap 6 7  8

---
+++ Chapter08/src/main/java/com/packt/webstore/domain/repository/ProductRepository.java
@@ -0,0 +1,23 @@
+package com.packt.webstore.domain.repository;
+
+import java.util.List;
+import java.util.Map;
+
+import com.packt.webstore.domain.Product;
+
+/**
+ * Created by chinhvd on 11/22/17.
+ */
+public interface ProductRepository {
+    List<Product> getAllProducts();
+
+    void updateStock(String productId, long noOfUnits);
+
+    List<Product> getProductsByCategory(String category);
+
+    List<Product> getProductsByFilter(Map<String, List<String>> filterParams);
+
+    Product getProductById(String productID);
+
+    void addProduct(Product product);
+}
